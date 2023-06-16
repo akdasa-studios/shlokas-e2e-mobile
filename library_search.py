eeef9e2f63ff77deed3816153bfed5a1b3a37787
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-06-16 08:49:18 +0200

    library_search

---
+++ library_search.py
@@ -0,0 +1,12 @@
+
+from base_test_case import BaseTestCase
+
+
+class LibrarySearch(BaseTestCase):
+    """Tests for sign in"""
+
+    def test_search_by_number(self):
+        pass
+
+    def test_search_by_content(self):
+        pass
